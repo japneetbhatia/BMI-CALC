0b6392f0fd5adda98f716d199097c95df14ae4eb
japneetbhatia <japneetk1111@gmail.com>
2021-05-09 17:46:50 +0530

    final

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#003366");
+final Color accentHexColor = HexColor("#80bfff");
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bmi_calculator/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'BMI CALCULATOR',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
